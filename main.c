8070cb4dbd96ddfa24d311e8ee82a901ded2f977
GabrielR52 <gabriel.ivi@hotmail.com>
2019-07-11 14:42:14 -0400

    Adicionado várias verificações e a função fazer reserva

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
     void conectar();
     void fzreserva();
     int verificarCampo();
+    int validarCampo();
+    int verificarCPF();
+    void efetivarReserva();
 
     int main ()
 
@@ -47,6 +50,7 @@
         else
         {
             printf("Saindo...");
+            exit(0);
         }
     }
 
@@ -58,24 +62,25 @@
 
         // estrutura do menu.
 
-        printf("              Tif Palace HOTEL\n\n||             Menu Principal             ||\n||----------------------------------------||\n||                                        ||\n||      1 - Cadastrar Cliente.            ||\n||      2 - Buscar Cliente.               ||\n||      3 - fazer Reserva.                ||\n||      0 - Para Sair.                    ||\n||                                        ||\n||                                        ||\n");
+        printf("              Tif Palace HOTEL\n\n||             Menu Principal             ||\n||----------------------------------------||\n||                                        ||\n||      1 - Cadastrar Cliente.            ||\n||      2 - Buscar Cliente.               ||\n||      3 - fazer Reserva.                ||\n||      4 - Sobre                         ||\n||      0 - Para Sair.                    ||\n||                                        ||\n||                                        ||\n");
 
         printf("\t>>> ");
 
         do{
             scanf("%d", &opc);
 
-            if(opc < 0 || opc > 3)
+            if(opc < 0 || opc > 4)
             {
                 printf("Opção invalida\n\t>>>");
             }
 
-        } while (opc < 0 || opc > 3);
+        } while (opc < 0 || opc > 4);
 
         if (opc == 1)
 
         {
             cadastrar(cadastro, conexao);
+            voltar(conexao);
         }
         else
         if (opc == 2)
@@ -93,7 +98,17 @@
         else
         if (opc == 3)
         {
-            fzreserva();
+            fzreserva(cadastro, conexao);
+            voltar(conexao);
+        }
+        else
+        if (opc == 4){
+            system("cls");
+            printf("\a\t\t\tCRIADO POR: \n\n");
+            printf("\t\tGabriel Rodrigues\n");
+            printf("\t\tLucas Rodrigues\n");
+            printf("\t\tMatheus Andrade\n");
+            voltar(conexao);
         }
     }
 
@@ -136,31 +151,30 @@ void cadastrar(char cadastro[][100], MYSQL *conexao)
     strcat(ano, dia);
     strcpy(cadastro[1], ano);
 
+    int valido = 0;
+    int campoEmUso = 0;
 
-    printf("CPF: ");
-    scanf("%s", &cadastro[2]);
-
-    //verificar se CPF já está em uso
-    int valido = verificarCampo(conexao, "cpf", cadastro[2]);
-
-    while(valido != 0){
+    // pegar o CPF
+    do{
         printf("CPF: ");
         scanf("%s", &cadastro[2]);
-        valido = verificarCampo(conexao, "cpf", cadastro[2]);
+        // verificar se o CPF é válido
+        valido = validarCampo(cadastro[2], 11);
+        // verificar se o CPF já está em uso
+        campoEmUso = verificarCampo(conexao, "cadastro", "cpf", cadastro[2]);
     }
+    while(valido != 1 || campoEmUso != 0);
 
-    printf("RG: ");
-    scanf("%s", &cadastro[3]);
-
-    //verificar se RG já está em uso
-    valido = verificarCampo(conexao, "rg", cadastro[3]);
-
-    while(valido != 0){
+    // pegar o RG
+    do{
         printf("RG: ");
         scanf("%s", &cadastro[3]);
-        valido = verificarCampo(conexao, "rg", cadastro[3]);
+        //verificar se o RG é válido
+        valido = validarCampo(cadastro[3], 9);
+        //verificar se o RG já está em uso
+        campoEmUso = verificarCampo(conexao, "cadastro", "rg", cadastro[3]);
     }
-
+    while(valido != 1 || campoEmUso != 0);
 
     fflush(stdin);
     printf("Cidade: ");
@@ -187,23 +201,21 @@ void cadastrar(char cadastro[][100], MYSQL *conexao)
             , cadastro[0], cadastro[2], cadastro[3], cadastro[1], cadastro[7], cadastro[4], cadastro[5], cadastro[6]);
 
 	mysql_query(conexao, query);
-
-	voltar(conexao);
 }
 
 void consultar (MYSQL *conexao)
 {
     system("cls");
     printf("CONSULTAR CLIENTES:\n");
-    char nome[100];
+    char cpf[100];
 
     fflush(stdin);
 
-    printf("Nome: ");
-    gets(nome);
+    printf("CPF: ");
+    gets(cpf);
 
     char query[100];
-    sprintf(query, "SELECT * FROM cadastro WHERE nome='%s'", nome);
+    sprintf(query, "SELECT * FROM cadastro WHERE cpf='%s'", cpf);
     if(mysql_query(conexao, query))
         printf("Erro\n");
     else
@@ -214,7 +226,7 @@ void consultar (MYSQL *conexao)
     int numero_linhas = mysql_num_rows(resp);
 
     if (numero_linhas == 0){
-        printf("Não encontrei %s", nome);
+        printf("Não encontrado CPF: %s", cpf);
     }else{
         char campos[9][100]={{"Id"}, {"Nome"}, {"CPF"}, {"RG"}, {"Nascimento"}, {"Telefone"}, {"Cidade"}, {"Estado"}, {"Pais"}};
         int i = 0;
@@ -224,42 +236,88 @@ void consultar (MYSQL *conexao)
             i++;
         }
     }
-
     voltar(conexao);
 }
 
-void fzreserva ()
+void fzreserva (char cadastro[][100], MYSQL *conexao)
 
 {
+    system("cls");
     char resp;
-    int id;
+
+    int cadastroExiste = 0;
+    char criarNovoCadastro = ' ';
+    char cpf[100] = {};
+    int novoCadastro = 0;
 
     printf ("O usuario ja é cadastrado (S ou N): ");
     scanf ("%s", &resp);
 
     if (resp == 'S' || resp == 's')
     {
+        printf("Digite seu CPF: ");
+        fflush(stdin);
+        gets(cpf);
+        cadastroExiste = verificarCampo(conexao, "cadastro", "cpf", cpf);
 
-        printf ("\nDigite o cpf do cliente:");
-        // Fazer consulta com o servidor.
+        // verifica se o CPF informado é válido, caso não seja, pergunta se a pessoa quer fazer um novo cadastro
+        if(cadastroExiste == 0){
+            printf("\nDesculpe, mas o CPF não foi encontrado!\n");
+            do{
+                printf("\nVocê quer se registrar? ");
+                scanf(" %c", &criarNovoCadastro);
+            }while(criarNovoCadastro != 's' && criarNovoCadastro != 'S' && criarNovoCadastro != 'n' && criarNovoCadastro != 'N');
+
+            //se sim, faz um novo cadastro
+            if(criarNovoCadastro == 'S' || criarNovoCadastro == 's'){
+                cadastrar(cadastro, conexao);
+                novoCadastro = 1;
+            }else{
+                voltar(conexao);
+            }
+        }
     }
     else
     if (resp == 'N' || resp == 'n')
 
     {
-        void cadastrar();
+        cadastrar(cadastro, conexao);
+        novoCadastro = 1;
     }
 
+    // caso houver um novo cadastro, ele já irá puxar do banco o último registro feito
+    if(novoCadastro == 1){
+        char query[100];
+        sprintf(query, "SELECT cpf FROM cadastro ORDER BY id DESC");
+        mysql_query(conexao, query);
+        MYSQL_RES *resultado = mysql_store_result(conexao);
+        MYSQL_ROW cpfRegistrado = mysql_fetch_row(resultado);
+        strcpy(cpf, cpfRegistrado[0]);
+    }
 
-    printf ("Digite o numero do Quarto:");
-    scanf ("%i", &id);
+    char id[3] = {};
+    int quartoEmUso;
+    int quartoNegativo = 0;
+
+    //verificar se o quarto digitado está disponível
+    do{
+        printf("\nDigite o numero do Quarto: ");
+        scanf("%s", &id);
+        quartoNegativo = (id[0] - 48);
+        if((quartoNegativo < 1)){
+            printf("\nQUARTO inválido\n");
+            exit(0);
+        }
+        quartoEmUso = verificarCampo(conexao, "quarto", "id", id);
+    }while(quartoEmUso == 1);
 
     //Verificar disponibilidade do quarto no Banco de dados.
+    efetivarReserva(conexao, id, cpf);
 }
 
-int verificarCampo(MYSQL *conexao, char field[100], char value[100]){
+int verificarCampo(MYSQL *conexao, char table[100], char field[100], char value[100]){
     char query[100];
-    sprintf(query, "SELECT * FROM cadastro WHERE %s='%s'", field, value);
+    sprintf(query, "SELECT * FROM %s WHERE %s='%s'", table, field, value);
 
     mysql_query(conexao, query);
 
@@ -267,9 +325,7 @@ int verificarCampo(MYSQL *conexao, char field[100], char value[100]){
 
     int linhas = mysql_num_rows(resultado);
 
-    if(linhas == 0){
-        printf("\n%s válido\n\n", field);
-    }else{
+    if(linhas != 0){
         printf("\n%s já está em uso!\n\n", field);
     }
 
@@ -279,3 +335,78 @@ int verificarCampo(MYSQL *conexao, char field[100], char value[100]){
     return linhas;
 }
 
+int verificarCPF(char valor[]){
+    int verificador, posicao, vezes = 0, vezes2, correto=0;
+    while(vezes<2){
+        verificador=0;
+        if (vezes==0){
+            posicao=10;
+            vezes2=posicao-1;
+        }else
+            posicao=11;
+            vezes2=posicao-1;
+
+        for(int i = 0; i < vezes2; i++){
+            //Uma maneira fácil de converter char para int é subtraindo -48 pois o 0 é 48
+            verificador += (valor[i]-48) * posicao;
+            posicao--;
+        }
+        verificador =  11 - verificador % 11;
+        if(verificador>9)
+            verificador=0;
+        if(verificador==valor[vezes+9]-48)
+            correto++;
+        vezes++;
+    }
+    if (correto == 2)
+        return 1;
+    else{
+        printf("CPF Inválido\n");
+        return 0;
+    }
+
+}
+int validarCampo(char valor[], int tamanho){
+    if(tamanho == 11){
+        //Verifica se foi digitado apenas números
+        for (int i = 0; i < tamanho; i++)
+            if (valor[i]>57||valor[i]<48){
+                printf("CPF Inválido\n");
+                return 0;
+            }
+        if(strlen(valor) == 11){
+            int valido = verificarCPF(valor);
+            if (valido == 0)
+                return 0;
+            else
+                return 1;
+        }else if(strlen(valor) > 11){
+            printf("\nCPF COM DIGITOS A MAIS\n");
+            return 0;
+        }
+    }else if(tamanho == 9){
+        //Verifica se foi digitado apenas números
+        for (int i = 0; i < tamanho; i++)
+            if (valor[i]>57||valor[i]<48){
+                printf("RG Inválido\n");
+                return 0;
+            }
+        if(strlen(valor) == 9){
+            return 1;
+        }else if(strlen(valor) > 9){
+            printf("\nRG COM DIGITOS A MAIS\n");
+            return 0;
+        }
+    }
+}
+
+void efetivarReserva(MYSQL *conexao, char id[3], char cpf[100]){
+    char query[100];
+    sprintf(query, "INSERT INTO quarto (id, ocupacao, pessoa) VALUES ('%s', 'S', '%s')", id, cpf);
+    mysql_query(conexao, query);
+
+    system("cls");
+    printf("\nRESERVA CONCLUIDA!\n");
+    printf("Cadastro: %s\n", cpf);
+    printf("Quarto: %s\n", id);
+}
